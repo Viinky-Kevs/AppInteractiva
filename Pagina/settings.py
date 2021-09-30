da7c2d23b81280749958f2b88bd016ebcf2987a2
Kevs <84353351+Viinky-Kevs@users.noreply.github.com>
2021-09-30 15:57:00 -0500

    Update settings.py

--- Pagina/settings.py
+++ Pagina/settings.py
@@ -74,13 +74,25 @@ WSGI_APPLICATION = 'Pagina.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'datosDB',
+        'USER': 'kevin',
+        'PASSWORD': 'Estand27',
+        'HOST': 'localhost',
+        'PORT': '',
+        }
+    }
 
 
 # Password validation
